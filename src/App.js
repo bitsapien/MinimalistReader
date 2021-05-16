3e193a8497105cb9e21d573a476f6caaee9f3475
bitsapien <bitsapien@gmail.com>
2021-05-16 17:18:54 +0530

    Add Insta kind of feed
    https://github.com/bitsapien/MinimalistReader/projects/1#card-60218028

--- src/App.js
+++ src/App.js
@@ -1,86 +1,58 @@
 import { useState, useEffect } from 'react'
-import rssToJson from './rssToJson'
-// State
-const subscriptions = [
-  {
-    name: 'Hacker News',
-    id: 'hacker-news',
-    src: 'https://hnrss.org/frontpage'
-  },
-  {
-    name: 'Paul Graham',
-    id: 'paul-graham',
-    src: 'https://hnrss.org/newest'
+import fetcher from './fetcher'
+
+const ShowPost = ({ post }) => {
+  const [postData, setPostData] = useState({})
+
+  const getPost = async() => {
+    const data = await post
+    console.log(data)
+    setPostData(data)
   }
-]
 
-const subscriptionFeeds = [
-  {
-    id: '1111',
-    title: 'How can I resolve “502 Bad Gateway Nginx/1.14.0 (Ubuntu)”?',
-    link: 'https://www.digitalocean.com/community/questions/how-can-i-resolve-502-bad-gateway-nginx-1-14-0-ubuntu',
-    description: {
-      pubDate: 'Sat, 01 May 2021 04:12:49 +0000',
-    }
-  },
-  {
-    id: '1114',
-    title: 'Mocked external Page',
-    link: 'http://localhost:3001/iframe-content',
-    description: {
-      pubDate: 'Sat, 01 May 2021 04:12:49 +0000',
-    }
-  }
-]
-
-// Subscriptions
-const Subscriptions = ({ subscriptions, setfeedUrl }) => {
-  const subscriptionLinksSet = subscriptionLinks(subscriptions, setfeedUrl)
-  return (<nav> <ul> {subscriptionLinksSet} </ul> </nav>)
-}
-
-const subscriptionLinks = (subscriptions, setfeedUrl) =>{
-  return subscriptions.map(subscription => (<li key={subscription.id}><span onClick={() => setfeedUrl(subscription.src)}> {subscription.name} </span></li>))
-}
-// Feed List
-const SubscriptionFeed = ({ feedUrl }) => {
-  const [feed, setFeed] = useState('')
-  const [isLoading, setIsLoading] = useState(true)
   useEffect(() => {
-    const proxy = 'http://localhost:8080/'
-    const fetchFeed = async() => {
-      setIsLoading(true)
-      const response = await fetch(proxy + feedUrl)
-      if(response.ok) {
-        const responseTxt = await response.text()
-        setFeed(responseTxt)
-      }
-      setIsLoading(false)
-    }
-    fetchFeed()
-  }, [feedUrl])
+    getPost()
+  })
 
-  const loading = (<span> loading .. </span>)
-  const subscriptionFeeds = isLoading ? [] : rssToJson(feed)
+  const postImage = (imgLink, title) =>
+    imgLink ? (<img src={imgLink} alt={title} />) : ''
 
-  return (<ul> {isLoading ? loading : subscriptionFeedLinks(subscriptionFeeds)} </ul>)
+  const postPanel = ({ link }) => <a href={link} rel="noreferrer" target="_blank" title={link}> {link} </a>
+
+  const createPost = ({ title, description, openGraphData, source, link }) => (
+    <section>
+      <h4> {title} </h4>
+      <span> {source} </span>
+      {postImage(openGraphData['og:image'], title, description)}
+      <div className="panel"> {postPanel({ link })} </div>
+
+    </section>
+  )
+
+  const loadingPost = () => ''
+
+
+  return (<div> {postData.id ? createPost(postData) : loadingPost} </div>)
 }
-
-const subscriptionFeedLinks = subscriptionFeeds =>
-  subscriptionFeeds.map(subscriptionFeed => (<li key={subscriptionFeed.id}> <a href={subscriptionFeed.link}> {subscriptionFeed.title}</a> </li>))
 
 
 function App() {
-  const [feedUrl, setfeedUrl] = useState(subscriptions[0].src)
+  const [feed, setFeed] = useState([])
+
+  const fetchIt =  async() => {
+    const feedData = await fetcher('http://hnrss.org/newest').then(postFeed => postFeed)
+    setFeed(feedData)
+  }
+
+  useEffect(() => {
+    fetchIt()
+  }, [])
 
   return (
-    <div className="app">
-      <Subscriptions subscriptions={subscriptions} setfeedUrl={setfeedUrl}/>
-      <div className="right">
-        <SubscriptionFeed feedUrl={feedUrl}/>
-      </div>
+    <div className="feed">
+      {feed.map(post => <ShowPost post={post} />)}
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/fetcher.js
@@ -0,0 +1,57 @@
+
+const rssToJson = (feedXml, source) => {
+  const xml = new window.DOMParser().parseFromString(feedXml, "text/xml")
+  const items = Array.from(xml.querySelectorAll("item"));
+  const regex = /<!\[CDATA\[(.*)\]\]>/;
+  console.log(items[1])
+  return items.map((item, index) => ({
+    id: index,
+    title: item.querySelector('title').innerHTML.match(regex)[1],
+    link: item.querySelector('link').innerHTML,
+    description: item.querySelector('description').innerHTML.replaceAll('\n', '').match(regex)[1],
+    source
+  }))
+}
+
+const fetchOpenGraph = async(feed) => {
+  const feedy = await feed.map(async(item) => {
+    const page = await fetchViaProxy(item.link)
+    const pageHTML = new window.DOMParser().parseFromString(page, 'text/html')
+    const openGraphData = Array.from(pageHTML.querySelectorAll('meta'))
+      .filter(metaTag => metaTag.getAttribute('property') && metaTag.getAttribute('property').includes('og:'))
+      .map(metaTag => [metaTag.getAttribute('property'), metaTag.getAttribute('content')])
+      .reduce((obj, item) => {
+        obj[item[0]] = item[1]
+        return obj
+      },{})
+
+    const itemNew = Object.assign({}, item)
+    itemNew.openGraphData = openGraphData
+
+    return itemNew
+
+  })
+  return feedy
+}
+
+const fetchViaProxy = async(url) => {
+  const proxy = 'http://localhost:8080/'
+  const response = await fetch(proxy + url)
+  if(response.ok) {
+    return await response.text()
+  }
+}
+const fetchFeed = async(url) => {
+  // fetch feed
+  const result = await fetchViaProxy(url)
+
+  // convert to json
+  const feed = rssToJson(result, 'hackernews')
+  // fetch og
+  const feedWithOg = await fetchOpenGraph(feed)
+  // return feed
+  return feedWithOg
+
+}
+
+export default fetchFeed
