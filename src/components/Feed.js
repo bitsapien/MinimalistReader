3eaeb7743e42577d041801701df0b89306329156
bitsapien <bitsapien@gmail.com>
2021-05-28 08:40:56 +0530

    Rollback infinite scroll for now

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -4,24 +4,11 @@ import { useState } from 'react'
 
 const INTERACTIONS = 'interactions'
 const interactionsFromStore = readStore()[INTERACTIONS] || []
-const MINIMUM_POSTS = 10
-const INCREMENT_POSTS_ON_SCROLL = 10
 
 
 const Feed = ({feed}) => {
-  const handleScroll = (e) => {
-    const target = e.target.documentElement
-      const bottom = target.scrollHeight - target.scrollTop === target.clientHeight;
-      if (bottom) {
-        setLoadTill(loadTill + INCREMENT_POSTS_ON_SCROLL)
-      }
-  }
 
-  window.addEventListener('scroll', handleScroll)
-  const [loadTill, setLoadTill] = useState(MINIMUM_POSTS)
-
-
-  return feed.slice(0, loadTill).map((post, index) => <Post key={index} post={post} interactionsFromStore={interactionsFromStore}/>)
+  return feed.map((post, index) => <Post key={index} post={post} interactionsFromStore={interactionsFromStore}/>)
 }
 
 export default Feed
