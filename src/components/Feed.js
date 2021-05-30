6f60b788713d455fee2d03a6fd983e3b8728c54a
bitsapien <bitsapien@gmail.com>
2021-05-30 22:57:28 +0530

    Feature: Load only 50 posts on first load and make user load more

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -2,6 +2,7 @@ import Post from './Post'
 import { readStore } from '../store'
 import LazyLoad from 'react-lazyload'
 import { filterFeed } from '../filters'
+import { useState } from 'react'
 
 const INTERACTIONS = 'interactions'
 const interactionsFromStore = readStore()[INTERACTIONS] || []
@@ -13,16 +14,26 @@ const Feed = ({ feed, filters }) => {
   const filteredFeed = filters.length > 0 ? filterFeed(feed, filters) : feed
   const sortedAndFilteredFeed = filteredFeed.sort(sortByLatestFirst)
 
+  const totalPosts = sortedAndFilteredFeed.length
+  const [numberOfLoadedPosts, setNumberOfLoadedPosts] = useState(50)
+
+  const buttonOrFin = totalPosts > numberOfLoadedPosts ?
+      (<button
+        onClick={() => setNumberOfLoadedPosts(numberOfLoadedPosts + 10)}>
+          Load More
+        </button>)
+    : (<section className="you-are-done">
+        Fin.
+      </section>)
+
   return (
   <div>
-    {sortedAndFilteredFeed.map((post, index) => (
+    {sortedAndFilteredFeed.slice(0, numberOfLoadedPosts-1).map((post, index) => (
       <LazyLoad key={index} offset={200}>
         <Post post={post} interactionsFromStore={interactionsFromStore}/>
       </LazyLoad>
     ))}
-    <section className="you-are-done">
-      Fin.
-    </section>
+    {buttonOrFin}
   </div>)
 }
 
